8ddc582bc301c4e713be696555151d167a1c7f57
github username <github email address>
2019-03-17 23:40:50 -0400

    done.

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+require_relative "./post.rb"
+require 'pry'
+
+class Author
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @posts = []
+    end
+
+    def posts
+        @posts
+    end
+    
+    def add_post(post)
+        @posts << post
+        @@all << post
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        @posts << post
+        @@all << post
+        post.author = self
+    end
+
+    def self.post_count
+        @@all.count
+    end
+end
